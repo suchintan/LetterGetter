1c062c135de6329bb44d2ce09d13934409011a1c
Suchintan Singh <suchintansingh@gmail.com>
2014-01-22 19:18:57 -0500

    Created some basic data types and basic logic

---
+++ src/com/awesome/lettergetter/Word.java
@@ -0,0 +1,34 @@
+package com.awesome.lettergetter;
+
+public class Word {
+	private String word;
+	private String definition;
+	private int points;
+	
+	public Word(String word, String definition){
+		this.setWord(word);
+		this.setDefinition(definition);
+	}
+
+	public String getWord() {
+		return word;
+	}
+
+	public void setWord(String word) {
+		this.word = word;
+	}
+
+	public String getDefinition() {
+		return definition;
+	}
+
+	public void setDefinition(String definition) {
+		this.definition = definition;
+	}
+	
+	public int getPoints(){
+		return PointHelper.generatePoints(this.getWord());
+	}
+	
+	
+}
