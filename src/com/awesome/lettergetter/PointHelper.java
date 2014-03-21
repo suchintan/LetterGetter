11b43e5cb2448c161543e982d21aeafbd6f1d566
Suchintan Singh <suchintansingh@gmail.com>
2014-03-21 19:03:03 -0400

    Started working on the backend, finished hte Trie

--- src/com/awesome/lettergetter/PointHelper.java
+++ src/com/awesome/lettergetter/PointHelper.java
@@ -1,5 +1,7 @@
 package com.awesome.lettergetter;
 
+import com.awesome.lettergetter.enums.LETTERS;
+
 public class PointHelper {
 	public static int generatePoints(String word){
 		if(word == null){
@@ -12,7 +14,7 @@ public class PointHelper {
 			if(s-'A' >= 0 && s-'Z' <= 0){
 				//Letters[65-65] = Letters[0] = A. Works for all letters.
 				//The letter A has an ascii value of 65. Letters[0] = A, so if we do
-				points += Letters.values()[s-'A'].getScore(); 
+				points += LETTERS.values()[s-'A'].getScore(); 
 			}
 		} 
 		
