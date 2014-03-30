a1f38ba9deb036bf643dcd398468dd51111571ae
sdcosta <sdcosta@gmail.com>
2014-03-30 12:48:04 -0400

    Add on to previous commit on menu. Forgot to commit files.

---
+++ src/com/awesome/lettergetter/ShopActivity.java
@@ -0,0 +1,15 @@
+package com.awesome.lettergetter;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ShopActivity extends MenuActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_shop);
+	}
+
+}
