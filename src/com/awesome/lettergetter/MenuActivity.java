621615e83621651c94bde48f77c8dbe725503764
sdcosta <sdcosta@gmail.com>
2014-03-30 12:48:04 -0400

    Add on to previous commit on menu. Forgot to commit files.

---
+++ src/com/awesome/lettergetter/MenuActivity.java
@@ -0,0 +1,38 @@
+package com.awesome.lettergetter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MenuActivity extends Activity {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.letter_getter, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+    	//Changes between menu items
+    	switch(item.getItemId())
+    	{
+    	case R.id.main:
+    		startActivity(new Intent(this, LetterGetterActivity.class));
+		return true;
+    	case R.id.shop:
+    		startActivity(new Intent(this, ShopActivity.class));
+    	return true;
+    	
+    	case R.id.leaderboard:
+    		startActivity(new Intent(this, LeaderboardActivity.class));
+		return true;
+		
+		default:
+			return super.onOptionsItemSelected(item);
+    		
+    	}
+    }
+}
