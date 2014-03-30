aaad4d51f769fa7017fc5fbd81e4211673f44cd9
sdcosta <sdcosta@gmail.com>
2014-03-30 06:03:55 -0400

    Created floating menu (action bar).

--- src/com/awesome/lettergetter/LetterGetterActivity.java
+++ src/com/awesome/lettergetter/LetterGetterActivity.java
@@ -3,8 +3,10 @@ package com.awesome.lettergetter;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,22 +14,12 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
-public class LetterGetterActivity extends Activity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_letter_getter);
-        
-        populateButtons(1,11);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.letter_getter, menu);
-        return true;
+public class LetterGetterActivity extends MenuActivity {
+	 @Override
+	    protected void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.activity_letter_getter);
+	        populateButtons(1,10);
     }
     
     public void search(View v){
