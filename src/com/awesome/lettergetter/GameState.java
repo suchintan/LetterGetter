0475909bb171f795aa3c18d28b30d5813150ed5c
sdcosta <sdcosta@gmail.com>
2014-04-01 18:51:48 -0400

    Implemented Shop items

--- src/com/awesome/lettergetter/GameState.java
+++ src/com/awesome/lettergetter/GameState.java
@@ -34,4 +34,38 @@ public class GameState {
 		
 		return leaders;
 	}
+	
+	//Creates shop items in gamestate and 
+	//returns a list of letters with cost
+	public List<LetterItem> getShopData(){
+		List<LetterItem> letters = new ArrayList<LetterItem>();
+		letters.add(new LetterItem("A", 2));
+		letters.add(new LetterItem("B", 3));
+		letters.add(new LetterItem("C", 3));
+		letters.add(new LetterItem("D", 2));
+		letters.add(new LetterItem("E", 1));
+		letters.add(new LetterItem("F", 4));
+		letters.add(new LetterItem("G", 2));
+		letters.add(new LetterItem("H", 4));
+		letters.add(new LetterItem("I", 1));
+		letters.add(new LetterItem("J", 8));
+		letters.add(new LetterItem("K", 5));
+		letters.add(new LetterItem("L", 1));
+		letters.add(new LetterItem("M", 3));
+		letters.add(new LetterItem("N", 1));
+		letters.add(new LetterItem("O", 1));
+		letters.add(new LetterItem("P", 3));
+		letters.add(new LetterItem("Q", 10));
+		letters.add(new LetterItem("R", 1));
+		letters.add(new LetterItem("S", 1));
+		letters.add(new LetterItem("T", 1));
+		letters.add(new LetterItem("U", 1));
+		letters.add(new LetterItem("V", 4));
+		letters.add(new LetterItem("W", 4));
+		letters.add(new LetterItem("X", 8));
+		letters.add(new LetterItem("Y", 4));
+		letters.add(new LetterItem("Z", 10));
+		
+		return letters;
+	}
 }
--- src/com/awesome/lettergetter/ShopActivity.java
+++ src/com/awesome/lettergetter/ShopActivity.java
@@ -3,6 +3,11 @@ package com.awesome.lettergetter;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class ShopActivity extends MenuActivity {
 
@@ -10,6 +15,44 @@ public class ShopActivity extends MenuActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_shop);
+		
+		populateShopView();
+	}
+	
+	private void populateShopView(){
+		ArrayAdapter<LetterItem> adapter = new ShopAdapter();
+		ListView list = (ListView) findViewById(R.id.shop_listview);
+		
+		list.setAdapter(adapter);
+	}
+	
+	private class ShopAdapter extends ArrayAdapter<LetterItem>{
+
+		public ShopAdapter() {
+			super(ShopActivity.this, R.layout.shop_view, gameState.getShopData());
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View shopeItemView = convertView;
+			if (shopeItemView == null){
+				shopeItemView = getLayoutInflater().inflate(R.layout.shop_view, parent, false);
+			}
+			//Find the leader
+			LetterItem currentLetter = gameState.getShopData().get(position);
+			
+			//Fill the view
+			//Letter:
+			TextView letterText = (TextView) shopeItemView.findViewById(R.id.shop_letter);
+			letterText.setText(currentLetter.getLetter());
+			
+			//Cost:
+			TextView costText = (TextView) shopeItemView.findViewById(R.id.shop_cost);
+			costText.setText("Cost: " + Integer.toString(currentLetter.getCost()));
+			
+			return shopeItemView;
+		}
+		
 	}
 
 }
